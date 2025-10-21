da0762953ec073f5680191e32781bed887f47b13
nachito02 <arguellojuan08@gmail.com>
2025-10-20 22:27:26 -0300

    emails send to mica

--- src/controllers/Email.controller.ts
+++ src/controllers/Email.controller.ts
@@ -3,13 +3,25 @@ import { sendEmailService } from "../services/emailService";
 
 export const sendEmail = async (req: Request, res: Response): Promise<void> => {
   const { to, subject, html } = req.body;
-  if (!to || !subject ||!html) {
+  if (!to || !subject || !html) {
     res.status(400).json({ error: "Missing required fields" });
     return;
   }
   try {
     const email = await sendEmailService(to, subject, html);
     console.log("Email sent: " + to, subject);
+
+    //send email to admin
+    await sendEmailService(
+      "mica@costaflores.com",
+      "new order",
+      `
+            <p>Se ha realizado una transacción con los siguientes datos</p>
+            <p><strong>Usuario:</strong> ${email}</p>
+            <p><strong>Asunto:</strong> ${subject}</p>
+            `
+    );
+
     res.status(200).json({ message: "Email sent successfully" });
     return;
   } catch (error) {
