f046b8a5416f9ecf4729739b9a1d408771a5a970
nachito02 <arguellojuan08@gmail.com>
2025-12-15 11:54:39 -0300

    email fix

--- src/controllers/Email.controller.ts
+++ src/controllers/Email.controller.ts
@@ -2,32 +2,24 @@ import { Request, Response } from "express";
 import { sendEmailService } from "../services/emailService";
 
 export const sendEmail = async (req: Request, res: Response): Promise<void> => {
-  const { to, subject, html } = req.body;
-  if (!to || !subject || !html) {
+  const { to, subject, html, wineryEmail } = req.body;
+
+  if (!to || !subject || !html || !wineryEmail) {
     res.status(400).json({ error: "Missing required fields" });
     return;
   }
-  try {
-    const email = await sendEmailService(to, subject, html);
-    console.log("Email sent: " + to, subject);
 
-    //send email to admin
-    await sendEmailService(
-      "mica@costaflores.com",
-      "new order",
-      `
-            <p>Se ha realizado una transacción con los siguientes datos</p>
-            <p><strong>Usuario:</strong> ${to}</p>
-            <p><strong>Asunto:</strong> ${subject}</p>
-            `
-    );
+  try {
+    // user email
+    await sendEmailService(to, subject, html);
+    console.log("Email sent:", to, subject);
+
+    // winery email
+    await sendEmailService(wineryEmail, subject, html);
 
     res.status(200).json({ message: "Email sent successfully" });
-    return;
   } catch (error) {
     console.error("Error sending email:", error);
-
     res.status(500).json({ error: "Error sending email", message: error });
-    return;
   }
 };
